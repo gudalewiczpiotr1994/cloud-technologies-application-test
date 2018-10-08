73702fe75f5c14f7850dffde119f4d699e1d1218
Piotr <gudalewiczpiotr@gmail.com>
2018-10-08 22:03:04 +0200

    First steps here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { StartSectionComponent } from './components/start-section/start-section.component';
+
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    StartSectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+      FormsModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
